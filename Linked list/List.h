e102b53f0d3882ba20c090732e50f2d2148c19ea
stefanded <50382034+stefanded@users.noreply.github.com>
2020-09-24 11:08:14 +0300

    Update List.h

--- Linked list/List.h
+++ Linked list/List.h
@@ -11,33 +11,6 @@ void StringListReplaceInStrings(char** head, char* before, char* after);
 
 void Swap(char** left, char** right);
 void StringListSort(char*** list);//buble sort
-//{
-//
-//    int swapped;
-//    char** ptr1;
-//    char** lptr = NULL;
-//
-//    /* Checking for empty list */
-//    if (list == NULL)
-//        return;
-//    do
-//    {
-//        swapped = 0;
-//        ptr1 = *list;
-//
-//        while (ptr1[1] != (char*)lptr)
-//        {
-//            char** next = (char**)ptr1[1];
-//            if (strcmp(ptr1[0], next[0]) > 0)
-//            {
-//                swap(&ptr1[0], &next[0]);
-//                swapped = 1;
-//            }
-//            ptr1 = next;
-//        }
-//        lptr = ptr1;
-//    } while (swapped);
-//}
 
 void PrintList(char** begin, char** end);
 int StringListIndexOf(char** begin, const char* str);
