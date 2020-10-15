85c4c0b22457e849968ea5cafaf215257626998c
stefanded <syxstep@gmail.com>
2020-10-15 04:56:35 +0300

    smart pointer

--- MyAny/MyAny.h
+++ MyAny/MyAny.h
@@ -1,18 +1,14 @@
-// This is a personal academic project. Dear PVS-Studio, please check it.
-
-// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
-
 #pragma once
 #include <typeindex>
 #include <sstream>
 #include <exception>
 #include <algorithm>
 
+#include <memory>
 
 class MyAny
 {
-	void* data_ = nullptr;
-	size_t size_type_ = 0;
+	std::shared_ptr<void> data_;
 	std::type_index type_ = std::type_index(typeid(void));
 public:
 
@@ -43,12 +39,9 @@ public:
 };
 
 template<class T>
-MyAny::MyAny(T value) : size_type_(sizeof(T)), type_(std::type_index(typeid(T)))
+MyAny::MyAny(T value) : data_(new T(value)), type_(std::type_index(typeid(T)))
 {
 	static_assert(std::is_fundamental<T>::value && !std::is_void<T>::value && !std::is_null_pointer<T>::value, "error type");
-	T* temp_data =new T(value);
-	*temp_data = value;
-	data_ = temp_data;
 }
 
 template<class T>
@@ -68,25 +61,16 @@ T MyAny::get_value() const
 
 	if (std::type_index temp_type = std::type_index(typeid(T)); temp_type == type_)
 	{
-		return *static_cast<T*>(data_);
+		return *static_cast<T*>(data_.get());
 	}
 	else
 	{
-
 		std::string msg{ std::string{"bad cast " } + type_.name() + std::string{"!="} + temp_type.name() };
 		throw std::runtime_error(msg.c_str());
 	}
 }
 
-MyAny::MyAny(const MyAny& any)
-{
-
-	data_ = std::malloc(any.size_type_);
-	std::memcpy(data_, any.data_, any.size_type_);
-	type_ = any.type_;
-	size_type_ = any.size_type_;
-
-}
+MyAny::MyAny(const MyAny& any) :data_(any.data_), type_(any.type_) {}
 
 MyAny& MyAny::operator=(const MyAny& any) noexcept
 {
@@ -110,17 +94,15 @@ const std::type_index& MyAny::type() const noexcept
 
 void MyAny::reset() noexcept
 {
-	delete data_;
+	data_.reset();
 	type_ = std::type_index(typeid(void));;
-	data_ = nullptr;
-	size_type_ = 0;
+
 }
 
 void MyAny::swap(MyAny& rhs)
 {
 	std::swap(data_, rhs.data_);
 	std::swap(type_, rhs.type_);
-	std::swap(size_type_, rhs.size_type_);
 
 }
 
